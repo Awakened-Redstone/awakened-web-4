91f1dabde60e66fc448b4064005bb37015583077
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2023-08-12 02:35:47 -0300

    chore: Separate code for readability chore: Remove kbar
    Signed-off-by: Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,49 +7,19 @@ import Head from "next/head";
 import {inter} from "@/system/utils";
 import classNames from "classnames";
 import React from "react";
-import {
-    Action,
-    KBarAnimator,
-    KBarPortal,
-    KBarPositioner,
-    KBarProvider,
-    KBarResults,
-    KBarSearch,
-    useMatches
-} from "kbar";
 import MainNavbar from "@/components/Navbar";
 
-function RenderResults() {
-    const {results} = useMatches();
-
+function Providers({children}: { children: React.ReactNode }) {
     return (
-        <KBarResults
-            items={results}
-            onRender={({item, active}) =>
-                typeof item === "string" ? (
-                    <div>{item}</div>
-                ) : (
-                    <div
-                        style={{
-                            background: active ? "#eee" : "transparent",
-                        }}
-                    >
-                        {item.name}
-                    </div>
-                )
-            }
-        />
-    );
+        <ThemeProvider attribute="class">
+            <NextUIProvider>
+                {children}
+            </NextUIProvider>
+        </ThemeProvider>
+    )
 }
 
 export default function App({Component, pageProps}: AppProps) {
-    const kbarActions: Action[] = [
-        {
-            id: 'mods',
-            name: 'Mods',
-            perform: () => alert("test"),
-        }
-    ];
 
     return (
         <>
@@ -58,43 +28,30 @@ export default function App({Component, pageProps}: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/assets/redstone_border.png"/>
             </Head>
-            <ThemeProvider attribute="class">
-                <NextUIProvider>
-                    <KBarProvider actions={kbarActions}>
-                        <KBarPortal>
-                            <KBarPositioner>
-                                <KBarAnimator>
-                                    <KBarSearch/>
-                                    <RenderResults/>
-                                </KBarAnimator>
-                            </KBarPositioner>
-                        </KBarPortal>
-                        <NextNProgress color={"#d30808"} stopDelayMs={0} height={2} options={{ showSpinner: false, speed: 100 }}/>
-                        {pageProps.navless ? null : <MainNavbar/>}
-                        <main
-                            className={
-                                classNames({
-                                    [`${inter.variable} font-base min-h-[60vh] overflow-x-hidden`]: true,
-                                    ["px-4 pt-4"]: !pageProps.borderless,
-                                })
-                            }
-                        >
-                            <Component {...pageProps} />
-                        </main>
-                        <Divider className="my-[10px] bg-[#3f3f46]"/>
-                        <footer className={`${inter.variable} font-base px-4 pb-4 w-full text-center`}>
-                            <div className={"mx-auto w-fit"}>
-                                Redistribution of my content outside of Modrinth or Github is prohibited unless you have
-                                my
-                                explicit permission.
-                            </div>
-                            <div className={"mx-auto w-fit opacity-20"}>Awakened Redstone © {
-                                new Date().getFullYear() === 2018 ? 2018 : `2018-${new Date().getFullYear()}`
-                            }</div>
-                        </footer>
-                    </KBarProvider>
-                </NextUIProvider>
-            </ThemeProvider>
+            <Providers>
+                <NextNProgress color={"#d30808"} stopDelayMs={0} height={2} options={{showSpinner: false, speed: 100}}/>
+                {pageProps.navless ? null : <MainNavbar/>}
+                <main
+                    className={
+                        classNames({
+                            [`${inter.variable} font-base min-h-[60vh] overflow-x-hidden`]: true,
+                            ["px-4 pt-4"]: !pageProps.borderless,
+                        })
+                    }
+                >
+                    <Component {...pageProps} />
+                </main>
+                <Divider className="my-[10px] bg-[#3f3f46]"/>
+                <footer className={`${inter.variable} font-base px-4 pb-4 w-full text-center`}>
+                    <div className={"mx-auto w-fit"}>
+                        Redistribution of my content outside of Modrinth or Github is prohibited unless you have
+                        my explicit permission.
+                    </div>
+                    <div className={"mx-auto w-fit opacity-20"}>Awakened Redstone © {
+                        new Date().getFullYear() === 2018 ? 2018 : `2018-${new Date().getFullYear()}`
+                    }</div>
+                </footer>
+            </Providers>
         </>
     )
 }
